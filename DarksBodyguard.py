bd6c39d42e896e34ec92631aee03c7c23ef6f186
xXTheDarkXx1000 <44132849+xXTheDarkXx1000@users.noreply.github.com>
2018-10-18 20:00:03 +0200

    Update DarksBodyguard.py

--- DarksBodyguard.py
+++ DarksBodyguard.py
@@ -64,7 +64,7 @@ async def on_message(message):
         randomlist = ["It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Perhaps","Signs point to yes","Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no.","My sources say no","Error Random response ID= 723 not found","It is certain","maybe :rikoshrug:",]
         await client.send_message(message.channel,(random.choice(randomlist)))
         import typing
-        @bot.command(!)
+        @bot.command('!')
         async def ban(ctx, members: commands.Greedy[discord.Member],
                    delete_days: typing.Optional[int] = 0, *,
                    reason: str):
