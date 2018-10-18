7813ef31ee8edf54ecfd1564c47c06a7832b4e27
xXTheDarkXx1000 <44132849+xXTheDarkXx1000@users.noreply.github.com>
2018-10-18 21:28:43 +0200

    Update DarksBodyguard.py

--- DarksBodyguard.py
+++ DarksBodyguard.py
@@ -51,8 +51,8 @@ async def on_message(message):
     if message.content == '!serverinvite':
         await client.send_message(message.channel,'https://discord.gg/3p7duB9')
     if message.content == '!help':
-        em = discord.Embed(description='Here you go <@%s> p.s this is a picture click on it to see the commands')
-        em.set_image(url='https://cdn.discordapp.com/attachments/500792101194301453/502549984613367839/help.PNG')  %(message.author.id))
+        em = discord.Embed(description='Here you go   p.s this is a picture click on it to see the commands')
+        em.set_image(url='https://cdn.discordapp.com/attachments/500792101194301453/502549984613367839/help.PNG')
         await client.send_message(message.channel, embed=em)
     if message.content.startswith('!1-100rng'):
         randomlist = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100",]
