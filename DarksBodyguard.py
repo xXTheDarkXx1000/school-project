9aa438ed6162735c793b8ea6f3cef314072fad52
xXTheDarkXx1000 <44132849+xXTheDarkXx1000@users.noreply.github.com>
2019-03-10 00:18:31 +0100

    Update DarksBodyguard.py

--- DarksBodyguard.py
+++ DarksBodyguard.py
@@ -49,7 +49,7 @@ async def on_message(message):
     if message.content.startswith('!!rob @Dark#3462'):
         await client.send_message(message.channel,':eyes: <@%s>!'  %(message.author.id))
     if message.content == '!serverinvite':
-        await client.send_message(message.channel,'https://discord.gg/3p7duB9')
+        await client.send_message(message.channel,'https://discord.gg/FTvmpU9')
     if message.content.startswith('!1-100rng'):
         randomlist = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100",]
         await client.send_message(message.channel,(random.choice(randomlist)))
@@ -61,4 +61,6 @@ async def on_message(message):
         await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content == '!sagiri':
         await client.send_message(message.channel,'https://cdn.discordapp.com/emojis/510523405628604428.png?v=1')
+    if message.content == '!help':
+        await client.send_message(message.channel,'https://cdn.discordapp.com/attachments/500792101194301453/554080105404825621/commandsfea.PNG')
 client.run('NTAwNzc2MjA0NTcyNTU3MzMz.DqZhzQ.Fojh7wDGPJuU0yxU378oXVbFInM')
