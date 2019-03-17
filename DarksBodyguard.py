7256dc2570a305310151e23bba3161e9e08a3c6b
xXTheDarkXx1000 <44132849+xXTheDarkXx1000@users.noreply.github.com>
2019-03-17 01:05:40 +0100

    Update DarksBodyguard.py

--- DarksBodyguard.py
+++ DarksBodyguard.py
@@ -61,4 +61,6 @@ async def on_message(message):
         await client.send_message(message.channel,'https://cdn.discordapp.com/emojis/510523405628604428.png?v=1')
     if message.content == '!help':
         await client.send_message(message.channel,'https://cdn.discordapp.com/attachments/500792101194301453/554080105404825621/commandsfea.PNG')
+    if message.content == '!bad':
+        await client.send_message(message.channel,'Welp you found the easteregg i (Dark) hid during the 17th of March 2019 at 1:04 AM CET this is supposed to mean that I am bad at coding and that most commands here are easy to re-create with almost no skill at all but im still learning one day i will get better at this!')
 client.run('NTAwNzc2MjA0NTcyNTU3MzMz.DqZhzQ.Fojh7wDGPJuU0yxU378oXVbFInM')
